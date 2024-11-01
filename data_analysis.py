381aa2bdf3d156e1953150f3bc0c74b88f14fb2b
Sijana <sijana.mamos@gmail.com>
2024-11-01 16:50:03 +0100

    Raw Data Cleaning

--- data_analysis.py
+++ data_analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 
 df = pd.read_csv("friends_dialogues.csv")
 
